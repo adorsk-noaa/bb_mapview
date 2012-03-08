9488746513f311f9139ddf4cf99308d29a913eb6
adorsk <adorsk>
2012-03-08 15:43:58 -0500

    added to mapview skeleton

--- src/views/mapview.js
+++ src/views/mapview.js
@@ -2,20 +2,35 @@ define([
 	"jquery",
 	"use!backbone",
 	"use!underscore",
+	"use!openlayers",
 	"text!./templates/mapview.html",
 		],
-function($, Backbone, _, template){
+function($, Backbone, _, ol, template){
 
 	var MapViewView = Backbone.View.extend({
 
 		initialize: function(){
-						console.log('yo');
 			this.render();
 		},
 
 		render: function(){
 			rendered_html = _.template(template);
 			$(this.el).html(rendered_html);
+
+			var map = new OpenLayers.Map({
+				div: $('.map', this.el).get(0)
+			});
+
+			var wms = new OpenLayers.Layer.WMS(
+				"OpenLayers WMS",
+				"http://vmap0.tiles.osgeo.org/wms/vmap0",
+				{
+					'layers': 'basic',
+					'params': 'kooka'
+				}
+				);
+			map.addLayer(wms);
+			map.zoomToMaxExtent();
 		}
 	});
 
