3e929e088e87530ddae34a5b4f732e82c1cd2556
adorsk <adorsk>
2012-05-22 15:51:41 -0400

    added map editor model

--- src/models/main.js
+++ src/models/main.js
@@ -1,14 +1,16 @@
 define([
 	'./mapview',
 	'./layer',
-	'./layer_collection'
+	'./layer_collection',
+	'./map_editor'
 ], 
-function(MapViewModel, LayerModel, LayerCollection){
+function(MapViewModel, LayerModel, LayerCollection, MapEditorModel){
 
 	var models = {
 		MapViewModel: MapViewModel,
 		LayerModel: LayerModel,
 		LayerCollection: LayerCollection,
+		MapEditorModel: MapEditorModel
 	};
 
 	return models;
---
+++ src/models/map_editor.js
@@ -0,0 +1,23 @@
+
+define([
+	"use!backbone",
+], 
+function(Backbone){
+
+var MapEditorModel = Backbone.Model.extend({
+
+	defaults: {
+		data_layer_definitions: [],
+		overlay_layer_definitions: [],
+		base_layer_definitions: []
+	},
+
+	initialize: function(){
+	}
+
+});
+
+return MapEditorModel;
+
+});
+
