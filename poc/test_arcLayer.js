21df18347126fb6806ef08537e217ca273227746
adorsk <adorsk>
2013-02-08 11:03:08 -0500

    testing out server resolutions for client zoom on habs layer

--- poc/test_arcLayer.js
+++ poc/test_arcLayer.js
@@ -35,6 +35,8 @@ require(
       cssDeferred.done(function(){
         console.log('styles loaded');
 
+        var resolutions = [156543.033928, 78271.5169639999, 39135.7584820001, 19567.8792409999, 9783.93962049996, 4891.96981024998, 2445.98490512499, 1222.99245256249, 611.49622628138, 305.748113140558, 152.874056570411, 76.4370282850732, 38.2185141425366, 19.1092570712683, 9.55462853563415, 4.77731426794937, 2.38865713397468];
+
         var overlayLayers = {
          Ocean: new Backbone.Model({
            layer_type: 'XYZ',
@@ -44,21 +46,40 @@ require(
            properties: new Backbone.Model({
              isBaseLayer: false,
              sphericalMercator: true,
-             resolutions: [156543.033928, 78271.5169639999, 39135.7584820001, 19567.8792409999, 9783.93962049996, 4891.96981024998, 2445.98490512499, 1222.99245256249, 611.49622628138, 305.748113140558, 152.874056570411],
+             serverResolutions: [156543.033928, 78271.5169639999, 39135.7584820001, 19567.8792409999, 9783.93962049996, 4891.96981024998, 2445.98490512499, 1222.99245256249, 611.49622628138, 305.748113140558, 152.874056570411, 76.4370282850732, 38.2185141425366, 19.1092570712683, 9.55462853563415, 4.77731426794937, 2.38865713397468],
+             minResolution: resolutions[10],
            })
          }),
          graticule: new Backbone.Model({
            layer_type:"Graticule",
            label:"Lat/Lon Grid",
            disabled: false,
-         })
+         }),
+         habs: new Backbone.Model({
+           layer_type: "WMS",
+           label:"habs",
+           url: 'http://localhost:8080/geoserver/topp/wms?',
+           disabled: false,
+           params: {
+             styles: 'habs',
+             layers: 'topp:habitats',
+             srs:'EPSG:3857',
+             transparent: true,
+           },
+           properties: new Backbone.Model({
+             projection: 'EPSG:3857',
+             serverResolutions: resolutions.slice(2,9),
+             visibility: true,
+             tileSize: new OpenLayers.Size(512, 512)
+           }),
+         }),
         };
 
         var defaultMap = new Backbone.Model({
           properties: new Backbone.Model({
             maxExtent: [-20037508.34, -20037508.34, 20037508.34, 20037508.34],
             extent: [-7792364.354444444, 3503549.8430166757, -7235766.900555555, 6446275.8401198285],
-            resolutions: [156543.033928, 78271.5169639999, 39135.7584820001, 19567.8792409999, 9783.93962049996, 4891.96981024998, 2445.98490512499, 1222.99245256249, 611.49622628138, 305.748113140558, 152.874056570411],
+            resolutions: resolutions,
             allOverlays: true,
             projection: 'EPSG:3857',
             displayProjection: 'EPSG:4326',
@@ -71,13 +92,35 @@ require(
             [overlayLayers.Ocean.clone()]
           ),
           overlay_layers: new Backbone.Collection(
-            [overlayLayers.graticule])
+            [
+              overlayLayers.graticule,
+              overlayLayers.habs
+          ])
         });
 
         mapEditor = new MapEditorView({
           model: mapEditorModel,
           el: $('#main'),
         });
+
+        var habs = window.mapEditor.mapView.map.layers[2];
+        boundsCache = {};
+        habs.getURL = function(bounds){
+          var q = OpenLayers.Layer.WMS.prototype.getURL.apply(this, arguments);
+          if (bounds){
+            strBounds = JSON.stringify(bounds.toArray());
+            if (boundsCache[strBounds]){
+              console.log('hit', strBounds);
+            }
+            else{
+              console.log('miss', strBounds);
+              boundsCache[strBounds] = strBounds;
+            }
+            console.log(bounds.toArray());
+          }
+          return q;
+        }
+
 
         mapEditor.trigger('ready');
 
